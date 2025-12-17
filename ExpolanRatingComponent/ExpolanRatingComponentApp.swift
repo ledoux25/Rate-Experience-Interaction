de2b0fc1b082fc7fec287cac5a3db3204d469995
ledoux25 <sanguojoseph17@gmail.com>
2025-12-16 21:33:01 -0500

    Initial Commit

---
+++ ExpolanRatingComponent/ExpolanRatingComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpolanRatingComponentApp.swift
+//  ExpolanRatingComponent
+//
+//  Created by Sanguo Joseph Ledoux on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpolanRatingComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpolanRatingComponentTests/ExpolanRatingComponentTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpolanRatingComponentTests.swift
+//  ExpolanRatingComponentTests
+//
+//  Created by Sanguo Joseph Ledoux on 12/16/25.
+//
+
+import Testing
+@testable import ExpolanRatingComponent
+
+struct ExpolanRatingComponentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
